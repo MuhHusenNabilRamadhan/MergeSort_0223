c0909b8d2c693c7712cc220c4fc6ac3dac02e837
MuhhusenNabilRamadhan <secondchoice230906@gmail.com>
2025-04-15 10:55:15 +0700

    prosedur while

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -3,3 +3,21 @@ using namespace std;
 
 int ar[20], B[20];
 int n;
+
+void input()
+{
+    while(true)
+    {
+        cout << "Masukkan Panjang element array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\n Maksimal panjang array adalah 20";
+        }
+    }
+}
